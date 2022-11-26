cb4fe5b424b4dad6245612fbc5121a303db79eed
Bklzn <58787195+Bklzn@users.noreply.github.com>
2022-11-26 14:36:02 +0100

    models (#2)

--- apkwww/blog/admin.py
+++ apkwww/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text_cut','author', 'created_date')
+    list_filter = ('author', 'created_date')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('author', 'created_date', 'published_date')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
