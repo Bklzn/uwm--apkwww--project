7e8a218ec9e4efb6632ebd12ffcdb2c0b42bd593
155972 <155972@student.uwm.edu.pl>
2022-12-02 23:07:31 +0100

    - add templates - add views: all posts, one post - add urls

--- apkwww/blog/views.py
+++ apkwww/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
+
 
 def index(request):
-    return HttpResponse("Hello world!")
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    return render(request, 'blog/post.html', {'post': post})
