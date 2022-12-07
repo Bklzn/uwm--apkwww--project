afce4ae47b59eed79f6e34b7e1616118cb81eee5
155972 <155972@student.uwm.edu.pl>
2022-12-07 23:02:59 +0100

    editing own posts, view users profiles, deleting posts and comments

--- apkwww/blog/views.py
+++ apkwww/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from .models import Post, Comment
-from .forms import CommentForm, PostForm
+from .forms import CommentForm, PostForm, EditPostForm
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -39,6 +40,14 @@ def post_view(request, pk):
                                               'user': request.user})
 
 
+def comment_delete(request, pk):
+    comment = Comment.objects.get(id=pk)
+    post = Post.objects.get(comment=comment)
+    if request.user.is_authenticated and post.author == request.user:
+        comment.delete()
+    return HttpResponseRedirect(f'/post/{post.id}')
+
+
 def post_create(request):
     if request.user.is_authenticated:
         form = PostForm(request.POST)
@@ -52,13 +61,59 @@ def post_create(request):
         return HttpResponse("Nie masz uprawnień, <a href='/api-authlogin'>zaloguj się</a>")
 
 
-def profile_view(request):
+def post_delete(request, pk):
+    if request.user.is_authenticated:
+        post = Post.objects.get(author=request.user, id=pk)
+        post.delete()
+        return HttpResponseRedirect('/')
+    else:
+        return HttpResponse("Nie masz uprawnień, <a href='/api-authlogin'>zaloguj się</a>")
+
+
+def post_edit(request, pk):
+    if request.user.is_authenticated:
+        post = Post.objects.get(author=request.user, id=pk)
+        if request.method == 'POST':
+            form = EditPostForm(request.POST)
+            if form.is_valid():
+                text = form.cleaned_data
+                if post.text != text['text']:
+                    post.text = text['text']
+                    if '[Edited]' not in post.title:
+                        post.title += '[Edited]'
+                    post.save()
+                return HttpResponseRedirect(f'/post/{post.id}')
+            return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
+        else:
+            form = EditPostForm(initial={'text': post.text})
+            return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
+    else:
+        return HttpResponse("Nie masz uprawnień, <a href='/api-authlogin'>zaloguj się</a>")
+
+
+def my_profile_view(request):
     if request.user.is_authenticated:
         posts = Post.objects.filter(author=request.user).order_by('-published_date')
         posts_num = posts.count()
         comments = Comment.objects.filter(author=request.user).order_by("-created_date")
         comments_num = comments.count()
-        return render(request, 'blog/profile.html', {'posts': posts, 'comments': comments})
+        return render(request, 'blog/profile.html', {'posts': posts,
+                                                     'comments': comments,
+                                                     'posts_num': posts_num,
+                                                     'comments_num': comments_num,
+                                                     })
     else:
         return HttpResponse("Nie masz uprawnień, <a href='/api-authlogin'>zaloguj się</a>")
 
+
+def profile_view(request, pk):
+    user_account = User.objects.get(id=pk)
+    posts = Post.objects.filter(author_id=pk).order_by('-published_date')
+    posts_num = posts.count()
+    comments = Comment.objects.filter(author_id=pk).order_by("-created_date")
+    comments_num = comments.count()
+    return render(request, 'blog/profile_view.html', {'posts': posts,
+                                                      'comments': comments,
+                                                      'posts_num': posts_num,
+                                                      'comments_num': comments_num,
+                                                      'user_account': user_account})
