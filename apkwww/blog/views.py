b129cb1c93ae35d76c8ae488df99efa3e39481a4
Bklzn <adrian_bon@wp.pl>
2022-12-08 15:46:35 +0100

    rest views

--- apkwww/blog/views.py
+++ apkwww/blog/views.py
@@ -2,42 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
 from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 from .forms import CommentForm, PostForm, EditPostForm
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def index(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/index.html', {'posts': posts,
-                                               'user': request.user})
+    if request.method == 'GET':
+        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+        serializer = PostSerializer(posts, many = True)
+        return Response(serializer.data)
 
-
+@api_view(['GET'])
 def post_view(request, pk):
     post = Post.objects.get(id=pk)
     comments = Comment.objects.filter(post_id=pk).order_by('-created_date')
-    num_of_comm = comments.count()
-    if request.method == 'POST':
-        form = CommentForm(request.POST, is_authenticated=request.user.is_authenticated)
-        if form.is_valid():
-            comment = form.cleaned_data
-            nickname = None
-            author = None
-            if request.user.is_authenticated:
-                author = request.user
-            elif request.user.is_anonymous and comment['nickname'] is not None:
-                nickname = comment['nickname']
-            # else:
-            #     return HttpResponse(400)
-            new_comment = Comment(text=comment['text'], post=post, author=author, nickname=nickname)
-            new_comment.save()
-            return HttpResponseRedirect(f'/post/{pk}')
-    else:
-        form = CommentForm()
-    return render(request, 'blog/post.html', {'post': post,
-                                              'comments': comments,
-                                              'num_of_comm': num_of_comm,
-                                              'form': form,
-                                              'user': request.user})
+    for com in comments:
+        com.text = com.text_cut()
+    serializerPost = PostSerializer(post)
+    serializerComments = CommentSerializer(comments, many = True)
+    return Response([serializerPost.data, serializerComments.data])
 
 
 def comment_delete(request, pk):
